f34b319b673e771911b0a9b5f4f00a5e11ad9b9a
GiovanniFlorez <giovanniflorez22@gmail.com>
2026-05-19 18:21:06 -0500

    Upgrade

--- pages/PQRSF.php
+++ pages/PQRSF.php
@@ -4,22 +4,22 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>PQRSF - UAnorte</title>
-        <link rel="icon" type="image/png" href="/assets/img/iconUAnorte.png">
-        <link rel="stylesheet" href="/assets/css/preloader.css">
-        <link rel="stylesheet" href="/assets/css/header.css">
-        <link rel="stylesheet" href="/assets/css/footer.css">
-        <link rel="stylesheet" href="/assets/css/FAB.css">
-        <link rel="stylesheet" href="/assets/css/PQRSF.css">
+        <link rel="icon" type="image/png" href="../assets/img/iconUAnorte.png">
+        <link rel="stylesheet" href="../assets/css/preloader.css">
+        <link rel="stylesheet" href="../assets/css/header.css">
+        <link rel="stylesheet" href="../assets/css/footer.css">
+        <link rel="stylesheet" href="../assets/css/FAB.css">
+        <link rel="stylesheet" href="../assets/css/PQRSF.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
         <link rel="stylesheet" href="https://fonts.cdnfonts.com/css/gilroy-bold">
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;1000;&display=swap" rel="stylesheet">
     </head>
     <body>
 <!-- =================================================== INCLUDES =================================================== -->
-        <?php include '/includes/preloader.php'; ?>
-        <?php require_once '/includes/config.php'; ?>
-        <?php include '/includes/header.php'; ?>
-        <?php include '/includes/FAB.php'; ?>
+        <?php include '../includes/preloader.php'; ?>
+        <?php require_once '../includes/config.php'; ?>
+        <?php include '../includes/header.php'; ?>
+        <?php include '../includes/FAB.php'; ?>
 
 <!-- =================================================== TEXT =================================================== -->
         <div class="text">
@@ -221,14 +221,14 @@
 
 <!-- =================================================== FOOTER =================================================== -->
         <div class="footer">
-            <?php include '/includes/footer.php'; ?>
+            <?php include '../includes/footer.php'; ?>
         </div>
 <!-- =================================================== SCRIPTS =================================================== -->
-        <script src="/assets/js/header.js"></script>
-        <script src="/assets/js/preloader.js"></script>
-        <script src="/assets/js/FAB.js"></script>
-        <script src="/assets/js/jsCheckBox.js"></script>
-        <script src="/assets/js/SweetAlert.js"></script>
+        <script src="../assets/js/header.js"></script>
+        <script src="../assets/js/preloader.js"></script>
+        <script src="../assets/js/FAB.js"></script>
+        <script src="../assets/js/jsCheckBox.js"></script>
+        <script src="../assets/js/SweetAlert.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     </body>
 </html>
